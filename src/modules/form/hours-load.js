25773eeba1fec7fa235ca4351ac1dc449a5ecc62
Lucas Figueiredo <olucasfg@outlook.com>
2025-01-24 19:38:20 -0300

    renderizando os horários

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,21 +1,34 @@
 import dayjs from "dayjs"
 
-import {openingHours} from "../../utils/opening-hours.js"
+import { openingHours } from "../../utils/opening-hours.js"
 
-export function hoursLoad({date}) {
+const hours = document.getElementById("hours")
+
+export function hoursLoad({ date }) {
   const opening = openingHours.map((hour) => {
     //recupera somente a hora
     const [scheduleHour] = hour.split(":")
-    
+
     //adiciona a hora na data e verifica se esta no passado
     const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
-    
-    return{
+
+    return {
       hour,
       available: isHourPast,
     }
 
   })
 
-  console.log(opening)
+  //renderiza os horários
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li")
+
+    li.classList.add("hour")
+    li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+    li.textContent = hour
+    hours.append(li)
+
+  })
+
 }
